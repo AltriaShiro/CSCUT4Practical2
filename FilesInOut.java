be2d6e6c2f1b8841b1d21bc1b48000d954da1dd5
Nesias Iverson Lirasan <89302874+AltriaShiro@users.noreply.github.com>
2023-04-10 11:42:40 +0400

    Update FilesInOut.java

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,65 +1,65 @@
-	import java.io.*;
-	import java.util.*;
+import java.io.*;
+import java.util.*;
 
-	public class FilesInOut {
-		public static void main(String[] args) {
-			// Read input file
-			File inputFile = new File("C:\\Users\\Cris\\git\\CSCUT4Practical2\\input.txt");
-			Scanner scanner = null;
+public class FilesInOut {
+    public static void main(String[] args) {
+        // Read input file
+        File inputFile = new File("C:\\Users\\Cris\\git\\CSCUT4Practical2\\input.txt");
+        Scanner scanner = null;
 
-			try {
-				scanner = new Scanner(inputFile);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+        try {
+            scanner = new Scanner(inputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-			// Format names and dates
-			List<String> formattedLines = new ArrayList<>();
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				String[] words = line.split(" ");
-				StringBuilder formattedName = new StringBuilder();
-				String formattedDate = "";
+        // Format names and dates
+        List<String> formattedLines = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] words = line.split(" ");
+            StringBuilder formattedName = new StringBuilder();
+            String formattedDate = "";
 
-				// Format name
-				for (int i = 0; i < words.length; i++) {
-					String word = words[i];
+            // Format name
+            for (int i = 0; i < words.length; i++) {
+                String word = words[i];
 
-					if (i == 0) {
-						formattedName.append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
-					} else if (i == 1 && word.length() == 2) {
-						formattedName.append(". ").append(word.toUpperCase());
-					} else {
-						formattedName.append(" ").append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
-					}
-				}
+                if (i == 0) {
+                    formattedName.append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
+                } else if (i == 1 && word.length() == 2) {
+                    formattedName.append(". ").append(word.toUpperCase());
+                } else if (!word.matches("\\d+")) { // ignore numbers after the names
+                    formattedName.append(" ").append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
+                }
+            }
 
-				// Format date
-				String dateString = line.substring(line.lastIndexOf(" ") + 1);
-				String day = dateString.substring(0, 2);
-				String month = dateString.substring(2, 4);
-				String year = dateString.substring(4);
-				formattedDate = day + "/" + month + "/" + year;
+            // Format date
+            String dateString = line.substring(line.lastIndexOf(" ") + 1);
+            String day = dateString.substring(0, 2);
+            String month = dateString.substring(2, 4);
+            String year = dateString.substring(4);
+            formattedDate = day + "/" + month + "/" + year;
 
-				formattedLines.add(formattedName.toString() + " " + formattedDate);
-			}
+            formattedLines.add(formattedName.toString() + " " + formattedDate);
+        }
 
-			scanner.close();
+        scanner.close();
 
-			// Write formatted output to file
-			File outputFile = new File("output.txt");
-			PrintWriter writer = null;
+        // Write formatted output to file
+        File outputFile = new File("output.txt");
+        PrintWriter writer = null;
 
-			try {
-				writer = new PrintWriter(outputFile);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+        try {
+            writer = new PrintWriter(outputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-			for (String formattedLine : formattedLines) {
-				writer.println(formattedLine);
-			}
+        for (String formattedLine : formattedLines) {
+            writer.println(formattedLine);
+        }
 
-			writer.close();
-		}
-	}
+        writer.close();
+    }
+}
