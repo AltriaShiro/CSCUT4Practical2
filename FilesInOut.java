a2bd82d1d4dc484865ede72630dba0d638e94082
Nesias Iverson Lirasan <89302874+AltriaShiro@users.noreply.github.com>
2023-04-10 11:38:38 +0400

    Update FilesInOut.java
    Name dates

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,57 +1,65 @@
-import java.io.*;
-import java.util.*;
+	import java.io.*;
+	import java.util.*;
 
-public class FilesInOut {
-    public static void main(String[] args) {
-        // Read input file
-        File inputFile = new File("C:\\Users\\Cris\\git\\CSCUT4Practical2\\input.txt");
-        Scanner scanner = null;
+	public class FilesInOut {
+		public static void main(String[] args) {
+			// Read input file
+			File inputFile = new File("C:\\Users\\Cris\\git\\CSCUT4Practical2\\input.txt");
+			Scanner scanner = null;
 
-        try {
-            scanner = new Scanner(inputFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+			try {
+				scanner = new Scanner(inputFile);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 
-        // Reading Names and Lines
-        List<String> formattedLines = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] words = line.split(" ");
-            StringBuilder formattedName = new StringBuilder();
-            
-            // Format Name
-            for (int i = 0; i < words.length; i++) {
-                String word = words[i];
+			// Format names and dates
+			List<String> formattedLines = new ArrayList<>();
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String[] words = line.split(" ");
+				StringBuilder formattedName = new StringBuilder();
+				String formattedDate = "";
 
-                if (i == 0) {
-                    formattedName.append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
-                } else if (i == 1 && word.length() == 2) {
-                    formattedName.append(". ").append(word.toUpperCase());
-                } else {
-                    formattedName.append(" ").append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
-                }
-            }
+				// Format name
+				for (int i = 0; i < words.length; i++) {
+					String word = words[i];
 
-            formattedLines.add(formattedName.toString());
-        }
+					if (i == 0) {
+						formattedName.append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
+					} else if (i == 1 && word.length() == 2) {
+						formattedName.append(". ").append(word.toUpperCase());
+					} else {
+						formattedName.append(" ").append(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
+					}
+				}
 
-        scanner.close();
+				// Format date
+				String dateString = line.substring(line.lastIndexOf(" ") + 1);
+				String day = dateString.substring(0, 2);
+				String month = dateString.substring(2, 4);
+				String year = dateString.substring(4);
+				formattedDate = day + "/" + month + "/" + year;
 
-        // Write formatted output to file
-        File outputFile = new File("output.txt");
-        PrintWriter writer = null;
+				formattedLines.add(formattedName.toString() + " " + formattedDate);
+			}
 
-        try {
-            writer = new PrintWriter(outputFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+			scanner.close();
 
-        for (String formattedLine : formattedLines) {
-            writer.println(formattedLine);
-        }
+			// Write formatted output to file
+			File outputFile = new File("output.txt");
+			PrintWriter writer = null;
 
-        writer.close();
-    }
-}
+			try {
+				writer = new PrintWriter(outputFile);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			for (String formattedLine : formattedLines) {
+				writer.println(formattedLine);
+			}
+
+			writer.close();
+		}
+	}
